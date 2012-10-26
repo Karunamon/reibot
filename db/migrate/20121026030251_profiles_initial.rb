d8cf47b3eaff24206d2faf4f32ca945649a73a0d
Michael Parks <tk@tkware.info>
2012-10-25 23:36:07 -0600

    what the hell was I thinking here activerecord does not work that way...

--- db/migrate/20121026030251_profiles_initial.rb
+++ db/migrate/20121026030251_profiles_initial.rb
@@ -1,33 +1,26 @@
 class ProfilesInitial < ActiveRecord::Migration
   def up
-    create_table :profile_owners do |t|
-      execute "CREATE TABLE profile_owners (
+    execute "CREATE TABLE profile_owners (
      id         INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,
      name       VARCHAR( 100 )  COLLATE 'NOCASE',
      netmasks   VARCHAR( 100 )  COLLATE 'NOCASE',
-     password   VARCHAR( 100 )
+     password   VARCHAR( 100 ),
      privileges VARCHAR( 100 )
 );"
-    end
-    create_table :profile_meta do |t|
-      execute "CREATE TABLE profile_meta (
+    execute "CREATE TABLE profile_meta (
       id        INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,
       title     VARCHAR( 100 )  COLLATE 'NOCASE',
       owner     INTEGER,
-      FOREIGN KEY (owner) REFERENCES profile_owners(id),
       timeset   DATETIME,
-      whoset    VARCHAR( 255 )  COLLATE 'NOCASE'
+      whoset    VARCHAR( 255 )  COLLATE 'NOCASE',
+      FOREIGN KEY (owner) REFERENCES profile_owners(id)
 );"
-    end
-    create_table :profile_data do |t|
-      execute "CREATE TABLE profile_data (
+    execute "CREATE TABLE profile_data (
     id          INTEGER        PRIMARY KEY AUTOINCREMENT,
     profile_id  INTEGER,
-    FOREIGN KEY (profile_id) REFERENCES profile_meta(id),
-    data      VARCHAR( 510 )
+    data      VARCHAR( 510 ),
+    FOREIGN KEY (profile_id) REFERENCES profile_meta(id)
 );"
-    end
-
   end
 
   def down
