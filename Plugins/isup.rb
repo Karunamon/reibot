e6293b7973878d098f3da23c8fc8431e0b3c400c
Michael Parks <tk@tkware.info>
2012-10-29 22:56:54 -0600

    Site up checker now works

--- Plugins/isup.rb
+++ Plugins/isup.rb
@@ -5,16 +5,32 @@ class Isup
   include Cinch::Plugin
   include RbUtils
 
-  match(/isup .*/, :prefix => "?", method: :upcheck)
+  match(/isup.*/, :prefix => "?", method: :upcheck)
 
   def upcheck(m)
-    msgarray = without_cmd(m.message)
-    page     =Net::HTTP.get('isup.me', msgarray[0])
-    if /not just you/ =~ page
-      m.reply "Looks like #{msgarray[0]} is down from here!"
+    m.reply ack_string
+    message  = m.message.split(" ")
+    response = fetch("http://isup.me/" + message[1]).body.inspect
+    if /not just you/ =~ response
+      m.reply "Looks like #{message[1]} is down from here!"
     else
-      m.reply "Looks like #{msgarray[0]} is up from here!"
+      m.reply "Looks like #{message[1]} is up from here!"
     end
   end
 
+  def fetch(uri_str, limit = 10)
+    raise ArgumentError, 'too many HTTP redirects' if limit == 0
+    response = Net::HTTP.get_response(URI(uri_str))
+
+    case response
+      when Net::HTTPSuccess then
+        response
+      when Net::HTTPRedirection then
+        location = response['location']
+        warn "redirected to #{location}"
+        fetch(location, limit - 1)
+      else
+        response.value
+    end
+  end
 end
