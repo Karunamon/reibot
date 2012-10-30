7591ca3633ec6e1162db5ed353dfc74edb2964c7
Michael Parks <tk@tkware.info>
2012-10-29 18:34:16 -0600

    Add a method to strip commands, rename string grabbers.

--- rb_utils.rb
+++ rb_utils.rb
@@ -4,19 +4,24 @@ module RbUtils
   #noinspection RubyResolve
   STRINGS=(YAML.load(File.read('strings.yml')))
 
-  def greeting
+  def greet_string
     STRINGS.fetch('greetings').to_a.sample
   end
 
-  def acknowledgement
+  def ack_string
     STRINGS.fetch('acknowledgements').to_a.sample
   end
 
-  def denial
+  def deny_string
     STRINGS.fetch('denials').to_a.sample
   end
 
-  def kick
+  def kick_string
     STRINGS.fetch('kicks').to_a.sample
+  end
+
+  #We're going to need to strip the first item from a line of text rather often
+  def without_cmd(incoming)
+    incoming.split(" ").delete_at(0)
   end
 end
