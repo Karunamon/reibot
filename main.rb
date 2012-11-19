48a1b6e6ad512615b14b48c951b5c3f55e80dd53
Michael Parks <tk@tkware.info>
2012-11-19 16:35:15 -0700

    Modify database config to be heroku compliant

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ end
 #Init the bot with our main config
 metabot=Cinch::Bot.new
 metabot.config.load(YAML.load(File.read('main.yml')))
-dbconfig=YAML.load(File.read('db/config.yml'))
+dbconfig=YAML.load(File.read('config/database.yml'))
 #For production use
 #metabot.loggers << Cinch::Logger::FormattedLogger.new(File.open("Logs/metabot.log", "a"))
 #metabot.loggers.level = :debug
@@ -17,7 +17,7 @@ dbconfig=YAML.load(File.read('db/config.yml'))
 
 #Database link
 ActiveRecord::Base.establish_connection(
-    dbconfig["development"]
+    dbconfig
 )
 #Boot up
 metabot.start
