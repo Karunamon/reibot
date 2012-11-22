2571f39e16cb12e02e8de1d7613803b6064afe24
Michael Parks <tk@tkware.info>
2012-11-22 10:54:59 -0700

    Profile importer now uses environment config

--- importprofiles.rb
+++ importprofiles.rb
@@ -1,3 +1,5 @@
+#importprofiles.rb
+#Quick script to load mIRC Reibot profiles.ini to your database of choice
 require 'cinch'
 require 'active_record'
 require 'pry'
@@ -5,11 +7,15 @@ require './rb_utils'
 require 'IniParse'
 require 'yaml'
 
+#Populate database config from the YML
 dbconfig=YAML.load(File.read('db/config.yml'))
 
-ActiveRecord::Base.establish_connection(
-    dbconfig["development"]
-)
+#If there's an environment variable set (ala Heroku), use that instead.
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  ActiveRecord::Base.establish_connection(dbconfig['development'])
+end
 
 class Owner < ActiveRecord::Base
   has_many :profiles
@@ -24,18 +30,17 @@ class Line < ActiveRecord::Base
   belongs_to :profiles
 end
 
-class InsertProfile
-end
+
 myfile       = File.read('profiles.ini')
 profile_file = IniParse.parse(myfile)
-
-#binding.pry
 profile_file.each do |section|
-  # binding.pry
   @owner     = Owner.find_or_create_by_name(profile_file[section.key]['SetBy'])
-  @profile   = @owner.profiles.create(:title => section.key, :whoset => profile_file[section.key]['SetBy'], :timeset => Time.parse(profile_file[section.key]['SetTime']))
+  @profile   = @owner.profiles.create(
+      :title => section.key,
+      :whoset => profile_file[section.key]['SetBy'],
+      :timeset => Time.parse(profile_file[section.key]['SetTime'])
+  )
   profilekey = 1
-  #binding.pry
   while profile_file[section.key][profilekey] do
     puts "I'm inserting #{profile_file[section.key][profilekey]} inside of #{section.key}"
     @profile.lines.create(:data => profile_file[section.key][profilekey])
